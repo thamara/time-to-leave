612604090f4da170799ff7531c4618eb016a3668
thamara <tkcandrade@gmail.com>
2019-09-22 16:25:01 -0300

    Changing notification to use node-notifier

--- js/notification.js
+++ js/notification.js
@@ -1,18 +1,30 @@
+const notifier = require('node-notifier');
+const path = require('path');
+
 /**
  * Notify user it's time to leave.
  */
 function notifyUser() {
-    if (Notification.permission !== 'granted')
-        Notification.requestPermission();
-    else {
-        var notification = new Notification('Time to leave', {
-            icon: 'assets/timer.png',
-            body: 'Hey there! I think it\'s time to leave.'
-        });
-
-        notification.onclose = function(){
-        };
-    }
+    notifier.notify(
+        {
+          title: 'Time to leave',
+          message: 'Hey there! I think it\'s time to leave.',
+          icon: path.join(__dirname, 'assets/timer.png'), // Absolute path (doesn't work on balloons)
+          sound: true, // Only Notification Center or Windows Toasters
+          wait: true // Wait with callback, until user action is taken against notification
+        },
+        function(err, response) {
+          // Response is response from notification
+        }
+      );
+       
+      notifier.on('click', function(notifierObject, options, event) {
+        // Triggers if `wait: true` and user clicks notification
+      });
+       
+      notifier.on('timeout', function(notifierObject, options) {
+        // Triggers if `wait: true` and notification closes
+      });
 }
 
 module.exports = {
