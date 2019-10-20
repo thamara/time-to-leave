b40960d81374fa391c092181c8abef48a278632f
Rodrigo Meyer <meyer20@users.noreply.github.com>
2019-10-19 23:44:39 -0300

    71 - Fixed notification icons path on Windows. (#82)
    * 71 - Fixed notification icons path on Windows. Needs to be tested on Mac
    
    * Remove console.log

--- js/notification.js
+++ js/notification.js
@@ -9,7 +9,7 @@ function notifyUser() {
         {
             title: 'Time to leave',
             message: 'Hey there! I think it\'s time to leave.',
-            icon: path.join(__dirname, 'assets/timer.png'), // Absolute path (doesn't work on balloons)
+            icon: path.join(path.dirname(require.main.filename), 'assets/timer.png'), // Absolute path (doesn't work on balloons)
             sound: true, // Only Notification Center or Windows Toasters
             wait: true
         },
@@ -21,7 +21,7 @@ function notify(msg) {
         {
             title: 'Time to leave',
             message: msg,
-            icon: path.join(__dirname, 'assets/timer.png'), // Absolute path (doesn't work on balloons)
+            icon: path.join(path.dirname(require.main.filename), 'assets/timer.png'), // Absolute path (doesn't work on balloons)
             sound: true, // Only Notification Center or Windows Toasters
             wait: true
         },
