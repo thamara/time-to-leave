dcc37de9c2b851e62b1f7aef381c60b43d0ff241
thamara <tkcandrade@gmail.com>
2019-12-12 00:12:31 -0300

    Fixing icon path in notification
    #155

--- js/notification.js
+++ js/notification.js
@@ -4,11 +4,14 @@ const path = require('path');
 const title = 'Time to Leave';
 
 function notify(msg) {
+    const appPath = process.env.NODE_ENV === 'production'
+            ? `${process.resourcesPath}/app`
+            : path.join(__dirname, '..');
     notifier.notify(
         {
             title: title,
             message: msg,
-            icon: path.join(path.dirname(require.main.filename), 'assets/timer.png'), // Absolute path (doesn't work on balloons)
+            icon: path.join(appPath, 'assets/timer.png'), // Absolute path (doesn't work on balloons)
             sound: true, // Only Notification Center or Windows Toasters
             wait: true
         },
