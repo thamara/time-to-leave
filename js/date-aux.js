f224a1aff31438ba1fbe964c546b2caafb4f7f72
Kashaiahyah85 <kashaiahyah85@icloud.com>
2020-03-06 13:05:13 -0800

    fixes for handling insane inputs

--- js/date-aux.js
+++ js/date-aux.js
@@ -2,7 +2,11 @@
  * Given a JS Date, return the string in the format YYYY-MM-DD.
  */
 function getDateStr(date) {
-    return new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
+    try {
+        return new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString().substr(0, 10);
+    } catch (err) {
+        return new Error(err);
+    }
 }
 
 module.exports = {
