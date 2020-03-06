4e8b07bacb50835c5d4f18dfb26fcd7cd971dc51
Kashaiahyah85 <kashaiahyah85@icloud.com>
2020-03-06 13:01:13 -0800

    return object instead of undefined

--- js/notification.js
+++ js/notification.js
@@ -5,18 +5,22 @@ const title = 'Time to Leave';
 
 function notify(msg) {
     const appPath = process.env.NODE_ENV === 'production'
-            ? `${process.resourcesPath}/app`
-            : path.join(__dirname, '..');
-    notifier.notify(
-        {
+        ? `${process.resourcesPath}/app`
+        : path.join(__dirname, '..');
+
+    return new Promise(resolve => {
+        notifier.notify({
             title: title,
             message: msg,
             icon: path.join(appPath, 'assets/timer.png'), // Absolute path (doesn't work on balloons)
             sound: true, // Only Notification Center or Windows Toasters
             wait: true
-        },
-    );
+        });
+        resolve(msg);
+    });
+
 }
+
 
 module.exports = {
     notify
