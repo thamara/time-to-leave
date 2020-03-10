6ee5d948de90fde10ad300951ffec161fb097bfa
Kashaiahyah85 <kashaiahyah85@icloud.com>
2020-03-10 10:00:00 -0700

    modified:   .gitignore 	modified:   js/import-export.js 	modified:   package-lock.json 	modified:   package.json

---
+++ __mocks__/fs.js
@@ -0,0 +1,22 @@
+const fs = jest.genMockFromModule("fs");
+const path = require("path");
+let mockFiles = Object.create(null);
+// example of newMockFiles
+// { "./testFolder/file1.txt": "This is the file content"
+function __createMockFiles(newMockFiles) {
+    mockFiles = Object.create(null);
+    for (const file in newMockFiles) {
+        const dir = path.dirname(file);
+        if (!mockFiles[dir]) {
+            mockFiles[dir] = [];
+        }
+        mockFiles[dir].push(path.basename(file));
+        mockFiles[dir][path.basename(file)] = newMockFiles[file];
+    }
+}
+function existsSync(pathToDirectory){
+    return mockFiles[pathToDirectory];
+}
+fs.existsSync = existsSync
+fs.__createMockFiles = __createMockFiles;
+module.exports = fs;
