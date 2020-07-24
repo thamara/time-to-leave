db8664d81af0e63e46995fd475349c9cb826d3df
Thamara Andrade <tkcandrade@gmail.com>
2020-07-24 00:25:52 -0300

    Improving the way we load the html for test

--- __tests__/__renderer__/workday-waiver.js
+++ __tests__/__renderer__/workday-waiver.js
@@ -1,5 +1,7 @@
 /* eslint-disable no-undef */
 const Store = require('electron-store');
+const fs = require('fs');
+const path = require('path');
 /* eslint-disable-next-line no-global-assign */
 $ = require('jquery');
 const { 
@@ -11,68 +13,11 @@ const {
 function prepareMockup() {
     const waivedWorkdays = new Store({ name: 'waived-workdays' });
     waivedWorkdays.clear();
-    // There gotta be a bettwe way of doing this, but I just not getting it :(
-    document.body.innerHTML = `
-        <body id="workday-waiver-window" class="common-window">
-            <div class="container">
-                <div class="header-title">
-                    Workday Waiver Manager
-                    <p class="header-help">Changes take effect when closing this window</p>
-                </div>
-
-                <section>
-                    <div class="section-title">Add Waiver</div>
-                    <form>
-                        <table>
-                            <tbody>
-                                <tr>
-                                    <td></td>
-                                    <th>Start date</th>
-                                    <td></td>
-                                    <th>End date</th>
-                                </tr>
-                                <tr>
-                                    <th>From</th>
-                                    <td><input id="start-date" type="date"></td>
-                                    <th>to</th>
-                                    <td><input id="end-date" type="date"></td>
-                                </tr>
-                                <tr>
-                                    <th>Hours</th>
-                                    <td>
-                                        <input type="text" id="hours" maxlength=8 pattern="^\\d+:\\d+$" placeholder="HH:mm" size=8>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <th>Reason</th>
-                                    <td colspan="3"><input id="reason" type="text" maxlength="30" size="60" placeholder="Reason for the waiver"></td>
-                                </tr>
-                            </tbody>
-                        </table>
-                        <br>
-                        <button class="waive-button" id="waive-button" type="button">
-                            Waive
-                        </button>
-                    </form>
-                </section>
-
-                <section>
-                    <div class="section-title">Waived workday list</div>
-                    <table id="waiver-list-table">
-                        <thead>
-                            <tr>
-                                <th>Day</th>
-                                <th>Waiver Reason</th>
-                                <th>Hours Waived</th>
-                                <th>Delete</th>
-                            </tr>
-                        </thead>
-                        <tbody></tbody>
-                    </table>
-                </section>
-            </div>
-        </body>
-    `;
+    const workdayWaiverHtml = path.join(__dirname, '../../src/workday-waiver.html');
+    const content = fs.readFileSync(workdayWaiverHtml);
+    var parser = new DOMParser();
+    var htmlDoc = parser.parseFromString(content, 'text/html');
+    document.body.innerHTML = htmlDoc.body.innerHTML;
 }
 
 function addTestWaiver(day, hours, reason) {
