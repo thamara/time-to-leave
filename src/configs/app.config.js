6b5901bd0ca231013717588cd65437617ef0af4e
Sem van Rij <sem@lekkerplakjekaas.nl>
2020-10-24 19:26:48 +0200

    Added 'nl' to app config

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    languages: ['en', 'pt-BR', 'es', 'it', 'zh-TW', 'de-DE','hi', 'mr'],
+    languages: ['en', 'pt-BR', 'es', 'it', 'zh-TW', 'de-DE', 'hi', 'mr', 'nl'],
     fallbackLng: 'en',
     namespace: 'translation'
 };
