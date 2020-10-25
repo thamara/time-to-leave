351e054acbbb64a1fcc4eb0bac8936404f8072e2
Esot3riA <esot3ria@gmail.com>
2020-10-25 17:03:27 +0900

    Add korean option on other locale files

--- src/configs/app.config.js
+++ src/configs/app.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    languages: ['en', 'pt-BR', 'es', 'it', 'zh-TW', 'de-DE','hi', 'mr', 'pl'],
+    languages: ['en', 'pt-BR', 'es', 'it', 'zh-TW', 'de-DE','hi', 'mr', 'pl', 'ko'],
     fallbackLng: 'en',
     namespace: 'translation'
 };
