ddca378547ba7225d8848d33a7aecafc7d3d07ad
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-02-10 16:32:47 +0100

    Adding animation customization & notification

--- website/dist/src.e31bb0bc.js
+++ website/dist/src.e31bb0bc.js
@@ -194,7 +194,43 @@ module.hot.accept(reloadCSS);
 
 require("./styles.scss");
 
-console.log("hello world!");
+gsap.registerPlugin(ScrollTrigger); // customization tl
+
+var tl = gsap.timeline({
+  // yes, we can add it to an entire timeline!
+  scrollTrigger: {
+    trigger: ".js-customization--trigger",
+    start: "top 90%",
+    // when the top of the trigger hits the top of the viewport
+    end: "+=500",
+    // end after scrolling 500px beyond the start
+    scrub: 1 // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+
+  }
+});
+tl.addLabel("start").from(".js-customization-image--3", {
+  x: 0,
+  y: 0
+}).from(".js-customization-image--2", {
+  x: 0,
+  y: 0
+}, "-=.3").addLabel("end"); // notification tl
+
+var nt = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".js-notification--trigger",
+    start: "top 80%",
+    // when the top of the trigger hits the top of the viewport
+    end: "+=400",
+    // end after scrolling 500px beyond the start
+    scrub: 1 // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+
+  }
+});
+nt.addLabel("start").from(".js-notification-image--2", {
+  x: 0,
+  y: 0
+}).addLabel("end");
 },{"./styles.scss":"styles.scss"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -223,7 +259,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59812" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59279" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- website/src/index.js
+++ website/src/index.js
@@ -1,2 +1,39 @@
 import "./styles.scss";
-console.log("hello world!");
+
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+// customization tl
+let tl = gsap.timeline({
+    // yes, we can add it to an entire timeline!
+    scrollTrigger: {
+      trigger: ".js-customization--trigger",
+      start: "top 90%", // when the top of the trigger hits the top of the viewport
+      end: "+=500", // end after scrolling 500px beyond the start
+      scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      
+    }
+  });
+
+  tl.addLabel("start")
+    .from(".js-customization-image--3", {x:0, y:0})
+    .from(".js-customization-image--2", {x:0, y:0}, "-=.3")
+    .addLabel("end");
+    
+
+// notification tl
+
+let nt = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".js-notification--trigger",
+        start: "top 80%", // when the top of the trigger hits the top of the viewport
+        end: "+=400", // end after scrolling 500px beyond the start
+        scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+        
+      }
+})
+
+nt.addLabel("start")
+    .from(".js-notification-image--2", {x:0, y:0})
+    .addLabel("end");
