54b68510e0d1b62f242a90279c025134f02701a8
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-02-11 15:27:59 +0100

    Updated buttons & added alt on images

--- website/dist/src.e31bb0bc.js
+++ website/dist/src.e31bb0bc.js
@@ -6331,7 +6331,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55608" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59587" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
