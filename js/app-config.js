36fefccdeeaccbd6e8aec4f99e6cc1398a6e1ad0
Guilherme Minozzi <61752597+Minozzzi@users.noreply.github.com>
2021-10-30 08:02:07 -0300

    Close #718: Use purple clock icon on tray and menus to enhance contrast
    On light/dark themes the icon combinations might almost be indistinguishable from the background.

--- js/app-config.js
+++ js/app-config.js
@@ -15,8 +15,8 @@ const appConfig = {
     macOS: macOS,
     win32: win32,
     appPath,
-    iconpath: path.join(__dirname, win32 ? '../assets/timer.ico' : '../assets/timer.png'),
-    trayIcon: path.join(__dirname, win32 ? '../assets/timer-grey.ico' : '../assets/timer-16-Template.png')
+    iconpath: path.join(__dirname, win32 ? '../assets/timer-purple.ico' : '../assets/timer-purple.png'),
+    trayIcon: path.join(__dirname, win32 ? '../assets/timer-purple.ico' : '../assets/timer-purple.png')
 };
 
 function getDetails()
