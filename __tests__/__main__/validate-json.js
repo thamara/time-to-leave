e3317593b389ed97531effa988d354f287b46b19
ge72mum <ge72mum@mytum.de>
2023-01-30 10:08:43 +0100

    simple validation + tests

--- __tests__/__main__/validate-json.js
+++ __tests__/__main__/validate-json.js
@@ -8,16 +8,124 @@ const {
 describe('Validate json', function()
 {
     process.env.NODE_ENV = 'test';
-
-    // TODO: Regular store entries are still here to test import of old dbs. Please remove on the next release.
     describe('validateJSON(instance)', function()
     {
-        const goodFlexibleEntry = {'type': 'flexible', 'date': '2020-06-03', 'values': ['08:00', '12:00', '13:00', '14:00']};
-        const goodWaivedEntry = {'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '08:00'};
+
+        describe('validate type', function()
+        {
+            const goodFlexibleEntry = [{ 'type': 'flexible', 'date': '2020-06-03', 'values': ['08:00', '12:00', '13:00', '14:00'] }];
+            const goodWaivedEntry = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '08:00' }];
+            const invalidType = [{ 'type': 'not valid type', 'date': '2020-06-03', 'values': ['08:00', '12:00', '13:00', '14:00'] }];
+            test('should be valid JSON', () =>
+            {
+                expect(validateJSON(goodWaivedEntry)).toBeTruthy();
+                expect(validateJSON(goodFlexibleEntry)).toBeTruthy();
+            });
+            test('should not be valid JSON', () =>
+            {
+                expect(validateJSON(invalidType)).not.toBeTruthy();
+            });
+        });
+
+        describe('validate date', function()
+        {
+            const goodFlexibleEntry = [{ 'type': 'flexible', 'date': '2020-06-03', 'values': ['08:00', '12:00', '13:00', '14:00'] }];
+            const goodWaivedEntry = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '08:00' }];
+            const invalidDateFormat = [{ 'type': 'flexible', 'date': '03-06-2020', 'values': ['08:00', '12:00', '13:00', '14:00'] }];
+            const invalidDateType = [{ 'type': 'flexible', 'date': ['2020-06-03'], 'values': ['08:00', '12:00', '13:00', '14:00'] }];
+            const invalidDateValue = [{ 'type': 'flexible', 'date': '2020-26-03', 'values': ['08:00', '12:00', '13:00', '14:00'] }];
+            // TODO
+            //const invalidDayInMonth = [{ 'type': 'flexible', 'date': '2020-02-31', 'values': ['08:00', '12:00', '13:00', '14:00'] }];
+            test('should be valid JSON', () =>
+            {
+                expect(validateJSON(goodWaivedEntry)).toBeTruthy();
+                expect(validateJSON(goodFlexibleEntry)).toBeTruthy();
+            });
+            test('should not be valid JSON', () =>
+            {
+                expect(validateJSON(invalidDateFormat)).not.toBeTruthy();
+                expect(validateJSON(invalidDateType)).not.toBeTruthy();
+                expect(validateJSON(invalidDateValue)).not.toBeTruthy();
+                //expect(validateJSON(invalidDayInMonth)).not.toBeTruthy();
+            });
+        });
+
+        describe('validate data', function()
+        {
+            const goodWaivedEntry = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '08:00' }];
+            const invalidDataType = [{ 'type': 'waived', 'date': '2020-06-03', 'data': ['waived'], 'hours': '08:00' }];
+            test('should be valid JSON', () =>
+            {
+                expect(validateJSON(goodWaivedEntry)).toBeTruthy();
+            });
+            test('should not be valid JSON', () =>
+            {
+                expect(validateJSON(invalidDataType)).not.toBeTruthy();
+            });
+        });
+
+        describe('validate hours', function()
+        {
+            const goodWaivedEntry = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '08:00' }];
+            const goodWaivedEntry2 = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '--:--' }];
+            const invalidHoursFormat = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '08-00' }];
+            const invalidHoursType = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': 8 }];
+            const invalidHoursValue = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '30:00' }];
+            const invalidHoursValueNegative = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '-01:00' }];
+            test('should be valid JSON', () =>
+            {
+                expect(validateJSON(goodWaivedEntry)).toBeTruthy();
+                expect(validateJSON(goodWaivedEntry2)).toBeTruthy();
+            });
+            test('should not be valid JSON', () =>
+            {
+                expect(validateJSON(invalidHoursFormat)).not.toBeTruthy();
+                expect(validateJSON(invalidHoursType)).not.toBeTruthy();
+                expect(validateJSON(invalidHoursValue)).not.toBeTruthy();
+                expect(validateJSON(invalidHoursValueNegative)).not.toBeTruthy();
+            });
+        });
+
+        describe('validate values', function()
+        {
+            const goodFlexibleEntry = [{ 'type': 'flexible', 'date': '2020-06-03', 'values': ['08:00', '12:00', '13:00', '14:00'] }];
+            const invalidValuesFormat1 = [{ 'type': 'flexible', 'date': '03-06-2020', 'values': ['0800', '12:00', '13:00', '14:00'] }];
+            const invalidValuesFormat2 = [{ 'type': 'flexible', 'date': '03-06-2020', 'values': ['08', '12:00', '13:00', '14:00'] }];
+            const invalidValuesFormat3 = [{ 'type': 'flexible', 'date': '03-06-2020', 'values': [8, '12:00', '13:00', '14:00'] }];
+            const invalidValuesFormat4 = [{ 'type': 'flexible', 'date': '03-06-2020', 'values': ['08-00', '12:00', '13:00', '14:00'] }];
+            const invalidValuesType = [{ 'type': 'flexible', 'date': ['2020-06-03'], 'values': '08:00'}];
+            const invalidValuesValue = [{ 'type': 'flexible', 'date': '2020-26-03', 'values': ['80:00', '12:00', '13:00', '14:00'] }];
+            // TODO
+            //const invalidPointsInTime = [{ 'type': 'flexible', 'date': '2020-02-31', 'values': ['08:00', '07:00', '13:00', '14:00'] }];
+            test('should be valid JSON', () =>
+            {
+                expect(validateJSON(goodFlexibleEntry)).toBeTruthy();
+            });
+            test('should not be valid JSON', () =>
+            {
+                expect(validateJSON(invalidValuesFormat1)).not.toBeTruthy();
+                expect(validateJSON(invalidValuesFormat2)).not.toBeTruthy();
+                expect(validateJSON(invalidValuesFormat3)).not.toBeTruthy();
+                expect(validateJSON(invalidValuesFormat4)).not.toBeTruthy();
+                expect(validateJSON(invalidValuesType)).not.toBeTruthy();
+                expect(validateJSON(invalidValuesValue)).not.toBeTruthy();
+                //expect(validateJSON(invalidPointsInTime)).not.toBeTruthy();
+            });
+        });
+
+
+        const goodFlexibleEntry = [{ 'type': 'flexible', 'date': '2020-06-03', 'values': ['08:00', '12:00', '13:00', '14:00'] }];
+        const goodWaivedEntry = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'waived', 'hours': '08:00' }];
+        const badFlexibleEntry = [{ 'type': 'flex', 'date': '2020-06-03', 'values': ['not-an-hour'] }];
+        const badFlexibleEntry2 = [{ 'type': 'flexible', 'date': '2020-06-03', 'values': 'not-an-array' }];
+        const badWaivedEntry = [{ 'type': 'waived', 'date': '2020-06-03', 'data': 'day-begin', 'hours': 'not-an-hour' }];
         test('should be valid', () =>
         {
             expect(validateJSON(goodWaivedEntry)).toBeTruthy();
             expect(validateJSON(goodFlexibleEntry)).toBeTruthy();
+            expect(validateJSON(badFlexibleEntry)).not.toBeTruthy();
+            expect(validateJSON(badFlexibleEntry2)).not.toBeTruthy();
+            expect(validateJSON(badWaivedEntry)).not.toBeTruthy();
         });
     });
 });
--- js/validate-json.js
+++ js/validate-json.js
@@ -1,40 +1,78 @@
 'use strict';
 
-var Validator = require('jsonschema').Validator;
+const Validator = require('jsonschema').Validator;
 
-var schema = {
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "type": "array",
-    "items": [
-        {
-            "type": "object",
-            "properties": {
-                "type": {
-                    "type": "string"
-                },
-                "date": {
-                    "type": "string"
-                },
-                "data": {
-                    "type": "string"
-                },
-                "hours": {
-                    "type": "string"
-                }
-            },
-            "required": [
-                "type",
-                "date",
-                "data",
-                "hours"
-            ]
+const schema = {
+    'id': '/singleEntry',
+    'type': 'array',
+    'items': {
+        oneOf: [
+            {'$ref': '/waivedEntry'},
+            {'$ref': '/flexibleEntry'}
+        ]
+    }
+};
+
+const schemaWaivedEntry = {
+    'id': '/waivedEntry',
+    'type': 'object',
+    'properties': {
+        'type':
+            {'type': 'string', 'pattern': 'waived' }
+        ,
+        'date': {
+            'type': 'string',
+            'pattern': /(1|2)[0-9]{3}-(0[1-9]{1}|1[0-1]{1})-(0[0-9]{1}|1[0-9]{1}|2[0-9]{1}|3[0-1]{1})$/
+        },
+        'data': {
+            'type': 'string'
+        },
+        'hours': {
+            'type': 'string',
+            'pattern': /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]|--:--$/
         }
+    },
+    'required': [
+        'type',
+        'date',
+        'data',
+        'hours'
     ]
-}
+};
 
-function validateJSON(instance){
-    var v = new Validator();
-    return v.validate(instance, schema);
+const schemaFlexibleEntry = {
+    'id': '/flexibleEntry',
+    'type': 'object',
+    'properties': {
+        'type':
+            {'type': 'string', 'pattern': 'flexible' }
+        ,
+        'date': {
+            'type': 'string',
+            'pattern': /(1|2)[0-9]{3}-(0[1-9]{1}|1[0-1]{1})-(0[0-9]{1}|1[0-9]{1}|2[0-9]{1}|3[0-1]{1})$/
+        },
+        'values': {
+            'type': 'array',
+            'items': {
+                'type': 'string',
+                'pattern': /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/
+            }
+        }
+    },
+    'required': [
+        'type',
+        'date',
+        'values'
+    ]
+};
+
+
+function validateJSON(instance)
+{
+    const v = new Validator();
+    v.addSchema(schemaFlexibleEntry, '/flexibleEntry');
+    v.addSchema(schemaWaivedEntry, '/waivedEntry');
+    return v.validate(instance, schema).valid;
 
 }
 
