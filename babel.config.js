ae8018e77084aa78eba9dbe2bb3c6ac621262bca
Arthur Araujo <araujoarthur0@hotmail.com>
2023-06-15 23:57:51 -0300

    Transforming import for jest to work

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    'presets': [
+        '@babel/preset-env'
+    ],
+    'env': {
+        'test': {
+            'plugins': [
+                '@babel/plugin-transform-modules-commonjs'
+            ]
+        }
+    }
+};
