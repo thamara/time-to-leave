ed01146f7e17534f628e82c5dc017e14556c6413
Arthur Araujo <araujoarthur0@hotmail.com>
2024-03-09 15:06:27 -0300

    Migrating date-aux.js

--- __tests__/electron-mocha-main.config.cjs
+++ __tests__/electron-mocha-main.config.cjs
@@ -1,5 +1,5 @@
 const data = require('./mocha-base.config.cjs');
 
-data.spec = ['__tests__/__main__/{time-math,validate-json}.js'];
+data.spec = ['__tests__/__main__/{date-aux,time-math,validate-json}.js'];
 
 module.exports = data;
