1b6aea64f0d837d2e31a277f4b68f6548c83bdd3
Arthur Araujo <araujoarthur0@hotmail.com>
2025-01-05 18:55:24 -0300

    Allowing one to override the electron-mocha test

--- __tests__/electron-mocha.config.cjs
+++ __tests__/electron-mocha.config.cjs
@@ -1,4 +1,11 @@
+function isTestFile(arg)
+{
+    return arg.match(/\.[jm]s?$/);
+}
+const allTestFiles = ['__tests__/__main__/*.mjs', '__tests__/__renderer__/*.mjs', '__tests__/__renderer__/classes/*.mjs'];
+
 module.exports = {
     color: true,
-    spec: ['__tests__/__main__/*.mjs', '__tests__/__renderer__/*.mjs', '__tests__/__renderer__/classes/*.mjs']
+    // This allows overriding the test on the cmd line when using this .cjs config file
+    ...(process.argv.some(isTestFile) ? {} : {spec: allTestFiles}),
 };
